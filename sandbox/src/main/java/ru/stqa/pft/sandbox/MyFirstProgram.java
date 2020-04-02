2d89e85c439c44b65a51640782979cb067792ab2
terekhin <igoryanterehin@gmail.com>
2020-04-03 00:20:00 +0400

    Random shit

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,5 +6,8 @@ AHAHAHA MEMASIKI PODKATILI
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("What's up, motherfuckers?");
+        int L = 4;
+        int S = L * L;
+        System.out.println("Area of a square with measure " + L + " = " + S + "m2");
     }
 }
