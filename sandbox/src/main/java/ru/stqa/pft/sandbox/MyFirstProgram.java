c4fe9304f5f4dfc2c14b63d498264c07c87f21da
terekhin <igoryanterehin@gmail.com>
2020-04-02 23:47:48 +0400

    Created package for MFP

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,8 @@ package ru.stqa.pft.sandbox;
 /*
 AHAHAHA MEMASIKI PODKATILI
  */
-public class MyFirstProgram
-{
-public static void main(String[] args) {
-	System.out.println("What's up, motherfuckers?");
-}
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("What's up, motherfuckers?");
+    }
 }
