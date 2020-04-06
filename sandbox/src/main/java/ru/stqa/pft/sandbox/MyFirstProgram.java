29f4d6edd64bf50109368a94ad157ee831934e1d
terekhin <igoryanterehin@gmail.com>
2020-04-07 00:01:39 +0400

    Added functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,7 +5,36 @@ AHAHAHA MEMASIKI PODKATILI
  */
 public class MyFirstProgram {
     public static void main(String[] args) {
-        String somebody = "motherfuckers";
-        System.out.println("What's up, " + somebody + "?");
+
+        hello("ПИДАРАСИНА");
+        hello("Moherfucker");
+
+
+        double l = 5;
+        System.out.println("Square's area with side " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 10;
+
+        System.out.println("Area with sides " + a + " and " + b + " = " + area(a, b));
+
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Howdy, " + somebody + "!");
+
+    }
+
+    public static double area(double len) {
+
+        return len * len;
+
+    }
+
+    public static double area(double a, double b) {
+
+        return a * b;
     }
 }
+
