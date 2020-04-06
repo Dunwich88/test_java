04c5f053acaf944f093fd52171bb2dfab37b0454
terekhin <igoryanterehin@gmail.com>
2020-04-06 23:25:10 +0400

    Added squaring to area formula

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,8 +7,5 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         String somebody = "motherfuckers";
         System.out.println("What's up, " + somebody + "?");
-        double L = 4.0;
-        double S = java.lang.Math.pow(L,2);
-        System.out.println("Area of a square with measure " + L + " = " + S + "m2");
     }
 }
