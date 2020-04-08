f40a559bc8198a94bda8a29a53919ec941da9494
terekhin <igoryanterehin@gmail.com>
2020-04-09 01:00:13 +0400

    Solved HT #1 completely (added method distance to Point.java and called it in MyFirstProgram.java)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.sandbox;
 
-import java.util.concurrent.RecursiveAction;
-
 public class MyFirstProgram {
     public static void main(String[] args) {
 
@@ -12,21 +10,21 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4, 10);
         System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + r.area());
 
-        Point p1 = new Point(1,4);
-        Point p2 = new Point(3,7);
-        System.out.println("Distance between two points = " + distance(p1,p2));
-
+        Point p1 = new Point(1, 4);
+        Point p2 = new Point(3, 7);
+       // System.out.println("Distance between two points — " + "point 1: " + p1.x1 + " " + p1.x2 + "; " +  "point 2: " + p2.x1 + " " + p2.x2 + "; " + " equals "  + distance(p1, p2));
+        System.out.println("Distance = " + p1.distance(p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Howdy, " + somebody + "!");
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((Math.pow((p1.x2 - p1.x1), 2) + Math.pow((p2.x2 - p2.x1), 2)));
+//    public static double distance(Point p1, Point p2) {
+//        return Math.sqrt((Math.pow((p1.x2 - p1.x1), 2) + Math.pow((p2.x2 - p2.x1), 2)));
     }
 
-        }
+
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,11 +4,16 @@ public class Point {
     public double x1;
     public double x2;
 
+
     public Point (double x1, double x2) {
 
         this.x1 = x1;
         this.x2 = x2;
-
     }
 
+    public double distance(Point otherPoint) {
+
+        return Math.sqrt((Math.pow((this.x2 - this.x1), 2) + Math.pow((otherPoint.x2 - otherPoint.x1), 2)));
+
+    }
 }
